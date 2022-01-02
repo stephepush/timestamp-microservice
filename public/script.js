8abb09ba4a1c656f73d82deeaa1bff114e8f7ce5
Stephen <7speter@gmail.com>
2022-01-01 21:28:51 -0500

    comments out renderTime and calls getTime to meet fcc project story

--- public/script.js
+++ public/script.js
@@ -1,9 +1,25 @@
-console.log("hello world")
+//console.log("hello world")
+
+
+
+const api_url = '/api/:date?';
 
 async function getTime() {
-    const response = await fetch('/api/:date?')
-    const json = await response.json()
-    document.getElementById("time").innerText = json
+    try {
+        const response = await fetch(api_url)
+        const time = await response.json()
+        console.log(time.utc)
+            //return time
+        return JSON.parse(time)
+    } catch (err) {
+        console.error(err)
+    }
+
 }
+
+/*async function renderTime() {
+    const element = document.getElementById("time");
+    element.innerText = await getTime()
+}*/
 
 getTime()
